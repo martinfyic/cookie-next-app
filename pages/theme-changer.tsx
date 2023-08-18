52344e090e3774ceaaa72d427ba311d04f973ab8
Martin Ferreira <martin.f.yic@gmail.com>
2023-08-17 21:57:09 -0300

    feat: :lipstick: add pages/theme-changer.tsx

---
+++ pages/theme-changer.tsx
@@ -0,0 +1,56 @@
+import { ChangeEvent, useState } from 'react';
+import { NextPage } from 'next';
+
+import {
+	Card,
+	CardContent,
+	FormControl,
+	FormControlLabel,
+	FormLabel,
+	Radio,
+	RadioGroup,
+} from '@mui/material';
+import Cookies from 'js-cookie';
+
+import { Layout } from '@/components';
+
+const ThemeChanger: NextPage = () => {
+	const [currentTheme, setCurrentTheme] = useState('light');
+
+	const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setCurrentTheme(event.target.value);
+	};
+
+	return (
+		<Layout>
+			<Card>
+				<CardContent>
+					<FormControl>
+						<FormLabel>Theme</FormLabel>
+						<RadioGroup
+							value={currentTheme}
+							onChange={onThemeChange}
+						>
+							<FormControlLabel
+								value='light'
+								control={<Radio />}
+								label='Light'
+							/>
+							<FormControlLabel
+								value='dark'
+								control={<Radio />}
+								label='Dark'
+							/>
+							<FormControlLabel
+								value='custom'
+								control={<Radio />}
+								label='Custom'
+							/>
+						</RadioGroup>
+					</FormControl>
+				</CardContent>
+			</Card>
+		</Layout>
+	);
+};
+export default ThemeChanger;
