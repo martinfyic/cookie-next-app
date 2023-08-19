d4b1d863b57e8b43be74e206532678133c7fd8f0
Martin Ferreira <martin.f.yic@gmail.com>
2023-08-19 19:52:44 -0300

    fin de seccion 11
    corregir bug con themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,42 @@
-import '@/styles/globals.css';
+import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
+
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Cookies from 'js-cookie';
+
+import { darkTheme, customTheme, lightTheme } from '@/themes';
+
+import '@/styles/globals.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { darkTheme, customTheme, lightTheme } from '@/themes';
 
-export default function App({ Component, pageProps }: AppProps) {
+interface Props extends AppProps {
+	theme: string;
+}
+
+function App({ Component, pageProps }: Props) {
+	const [currentTheme, setCurrentTheme] = useState(lightTheme);
+
+	useEffect(() => {
+		const cookieTheme = Cookies.get('theme') || 'light';
+		const selectedTheme =
+			cookieTheme === 'light'
+				? lightTheme
+				: cookieTheme === 'dark'
+				? darkTheme
+				: customTheme;
+		setCurrentTheme(selectedTheme);
+	}, []);
+
 	return (
-		<ThemeProvider theme={darkTheme}>
+		<ThemeProvider theme={currentTheme}>
 			<CssBaseline />
 			<Component {...pageProps} />
 		</ThemeProvider>
 	);
 }
+
+export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,43 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+// import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-	return (
-		<Html lang='es'>
-			<Head />
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+// export default function Document() {
+// 	return (
+// 		<Html lang='es'>
+// 			<Head />
+// 			<body>
+// 				<Main />
+// 				<NextScript />
+// 			</body>
+// 		</Html>
+// 	);
+// }
+
+import Document, {
+	DocumentContext,
+	Html,
+	Head,
+	Main,
+	NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const initalProps = await Document.getInitialProps(ctx);
+
+		return initalProps;
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
+
+export default MyDocument;
